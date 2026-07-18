8d39ccc00d762aa6689a3196f1a5951b273cccf6
prtrnjn <roy.prt@gmail.com>
2026-07-18 10:56:00 +0530

    Code missing in 1_EDA/03_top_optimal_skill.sql

--- 1_EDA/03_top_optimal_skill.sql
+++ 1_EDA/03_top_optimal_skill.sql
@@ -6,7 +6,28 @@ Create a ranking column that combines demand count and median salary to identity
 
 */
 
- 
+SELECT
+    sd.skills,
+    ROUND(MEDIAN (jpf. salary_year_avg), 0) AS median_salary,
+    ROUND(LN(COUNT (jpf.*)), 1) AS In_demand_count,
+    ROUND((MEDIAN(jpf. salary_year_avg) * LN(COUNT(jpf.*)))/1_000_000, 2) AS optimal_score
+FROM 
+    job_postings_fact AS jpf
+INNER JOIN skills_job_dim AS sjd
+    ON jpf.job_id = sjd. job_id
+INNER JOIN skills_dim AS sd
+    ON sjd.skill_id = sd.skill_id
+WHERE
+    jpf.job_title_short = 'Data Engineer'
+    AND jpf. job_work_from_home = True
+    AND jpf. salary_year_avg IS NOT NULL
+GROUP BY
+    sd. skills
+HAVING
+    COUNT (jpf.*) > 100
+ORDER BY
+    optimal_score DESC
+LIMIT 25;
 
 /*
 ────────────┬───────────────┬─────────────────┬───────────────┐
